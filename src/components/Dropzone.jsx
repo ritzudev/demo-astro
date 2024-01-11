767ded1d0b2f2372a303025ab200b5c69d3ddad2
Jimmy Alex Flores Zamudio <alex.flores@telemovilperu.com>
2024-01-10 22:53:25 -0500

    fix import lodash

--- src/components/Dropzone.jsx
+++ src/components/Dropzone.jsx
@@ -5,6 +5,8 @@ import { useDropzone } from 'react-dropzone'
 import imglyRemoveBackground from '@imgly/background-removal';
 import { ImgComparisonSlider } from '@img-comparison-slider/react';
 
+export const prerender = false
+
 const images = [
   'https://images.unsplash.com/photo-1686002359940-6a51b0d64f68?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1024&q=80',
   'https://images.unsplash.com/photo-1590523278191-995cbcda646b?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjEyMDd9'
@@ -48,12 +50,6 @@ export function DropZoneComponent() {
   const [seconds, setSeconds] = useState(0);
   const [startDate, setStartDate] = useState(Date.now());
   const [caption, setCaption] = useState('Click me to remove background');
-
-  let config = {
-    fetchArgs: {
-      mode: 'no-cors'
-    }
-  };
 
   useEffect(() => {
     let interval = null;
@@ -118,13 +114,13 @@ export function DropZoneComponent() {
           }
         `}
       </style>
-      { !acceptedFiles[0] ? (
+      { acceptedFiles[0] ? (
           <div className='p-4 bg-[#27272a] rounded-lg'>
             <ImgComparisonSlider className='slider-example-split-line' hover="hover">
-            {/* <img slot="first"  width="100%"  src={URL.createObjectURL(acceptedFiles[0])} />
-            <img slot="second" width="100%"  src={image} /> */}
-            <img slot="first"  width="100%"  src={image_src} />
-            <img slot="second" width="100%"  src={image_src} />
+            <img slot="first"  width="100%"  src={URL.createObjectURL(acceptedFiles[0])} />
+            <img slot="second" width="100%"  src={image} />
+            {/* <img slot="first"  width="100%"  src={image_src} />
+            <img slot="second" width="100%"  src={image_src} /> */}
           </ImgComparisonSlider>
           </div>
         ) : (
